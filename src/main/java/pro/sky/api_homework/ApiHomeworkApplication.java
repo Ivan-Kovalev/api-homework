cbd1a03b39ad0281ac8fdb12972c21c96158a107
Ivan-Kovalev <gangrenresdwa@gmail.com>
2024-06-12 17:26:52 +0300

    master

---
+++ src/main/java/pro/sky/api_homework/ApiHomeworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.api_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHomeworkApplication.class, args);
+	}
+
+}
